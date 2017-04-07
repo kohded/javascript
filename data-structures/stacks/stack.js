fc42345c4572bad8b1305c0c8b60218f91222a63
Arnold Koh <kohded@users.noreply.github.com>
2017-04-07 04:36:59 -0700

    Add stack

---
+++ data-structures/stacks/stack.js
@@ -0,0 +1,63 @@
+/**
+ * @author Arnold Koh <arnold@kohded.com>
+ * Developed: 4/7/2017
+ * File: stack.js
+ */
+
+(function () {
+  const Stack = function () {
+    this.stack = [];
+  };
+
+  Stack.prototype = {
+    /**
+     * Push an element on top of the stack.
+     * @param element The object to be added.
+     */
+    push(element) {
+      return this.stack.push(element);
+    },
+    /**
+     * Pop an element off the top of the stack.
+     * @returns {*|T} The object at the top of the stack.
+     */
+    pop() {
+      return this.stack.pop();
+    },
+    /**
+     * Peek at the element on the top of the stack without removal.
+     * @returns {*} The object at the top of the stack.
+     */
+    peek() {
+      return this.stack[this.stack.length - 1];
+    },
+    /**
+     * Return the number of elements in the stack.
+     * @returns {Number} The size of the stack.
+     */
+    size() {
+      return this.stack.length;
+    },
+    /**
+     * Check if the stack is empty.
+     * @returns {boolean} If empty returns true.
+     */
+    isEmpty() {
+      return this.stack.length === 0;
+    },
+    /**
+     * Clear all elements from the stack.
+     */
+    clear() {
+      this.stack = [];
+    },
+    /**
+     * Return string representation of the stack.
+     */
+    toString() {
+      return this.stack.toString();
+    }
+  };
+
+  module.exports = Stack;
+}());
---
+++ test/data-structures/stacks/stack-test.js
@@ -0,0 +1,71 @@
+/**
+ * @author Arnold Koh <arnold@kohded.com>
+ * Developed: 4/7/2017
+ * File: stack-test.js
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions */
+
+const expect = require('chai').expect;
+const Stack = require('../../../data-structures/stacks/stack.js');
+
+describe('Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+  });
+
+  describe('#push(element)', () => {
+    it('should return element that was pushed on the stack', () => {
+      expect(stack.push(4)).to.equal(4);
+    });
+  });
+
+  describe('#pop()', () => {
+    it('should return element that was popped off the stack', () => {
+      expect(stack.pop(3)).to.equal(3);
+      expect(stack.pop(2)).to.equal(2);
+      expect(stack.pop(1)).to.equal(1);
+    });
+  });
+
+  describe('#peek()', () => {
+    it('should return element on top of the stack without removal', () => {
+      expect(stack.peek()).to.equal(3);
+    });
+  });
+
+  describe('#size()', () => {
+    it('should return number of elements in the stack', () => {
+      expect(stack.size()).to.equal(3);
+    });
+  });
+
+  describe('#isEmpty()', () => {
+    it('should return false if the stack is not empty', () => {
+      expect(stack.isEmpty()).to.be.false;
+    });
+    it('should return true if the stack is empty', () => {
+      const emptyStack = new Stack();
+      expect(emptyStack.isEmpty()).to.be.true;
+    });
+  });
+
+  describe('#clear()', () => {
+    it('should return true from isEmpty after clearing the stack', () => {
+      stack.clear();
+      expect(stack.isEmpty()).to.be.true;
+    });
+  });
+
+  describe('#toString()', () => {
+    it('should return string representation of the stack', () => {
+      expect(stack.toString()).to.equal('1,2,3');
+    });
+  });
+});
